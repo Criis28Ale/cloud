1ddb9190feebbbeb987489e4e37771f363056702
Cristopher <cristopher.azurdia@galileo.edu>
2023-11-29 20:41:57 -0600

    first commit

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,6 +5,7 @@ const dbsetting = {
     password: 'root123-',
     server: 'database-2.cbklfbxzenuh.us-east-1.rds.amazonaws.com',
     database: 'cloud',
+    port: '1433',
     options: {
         encrypt: true,
         trustServerCertificate: true,
